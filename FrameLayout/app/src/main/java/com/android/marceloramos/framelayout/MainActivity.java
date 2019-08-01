82f4c257828b34c4486f097a12f0f1740f7db6fa
marcelo ramos <marceloramos_86@hotmail.com>
2019-07-31 22:40:52 -0300

    Aplicando FrameLayout

---
+++ FrameLayout/app/src/main/java/com/android/marceloramos/framelayout/MainActivity.java
@@ -0,0 +1,24 @@
+package com.android.marceloramos.framelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.frameCarregando);
+        frameLayout.setVisibility(View.GONE);
+    }
+
+    public void abrir(View view){
+        frameLayout.setVisibility(view.VISIBLE);
+    }
+}
