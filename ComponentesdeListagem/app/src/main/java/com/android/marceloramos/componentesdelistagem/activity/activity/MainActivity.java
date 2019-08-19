e8f220560c809724a63b258a668d5399ac423fdf
marcelo ramos <marceloramos_86@hotmail.com>
2019-08-19 11:46:07 -0300

    Implementando interface do RecyclerView

--- ComponentesdeListagem/app/src/main/java/com/android/marceloramos/componentesdelistagem/activity/activity/MainActivity.java
+++ ComponentesdeListagem/app/src/main/java/com/android/marceloramos/componentesdelistagem/activity/activity/MainActivity.java
@@ -1,28 +1,32 @@
-package com.android.marceloramos.componentesdelistagem;
+package com.android.marceloramos.componentesdelistagem.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.marceloramos.componentesdelistagem.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
     private ListView listLocais;
+
     private String[] itens = {
             "Angra dos Reis", "Caldas Novas", "Campos do Jordão",
             "Costa do Sauípe", "Ilhéus", "Porto Seguro",
             "Tiradentes", "Praga", "Santiago", "Zurique",
             "Caribe", "Buenos Aires", "Budapeste", "Cancún", "Aruba"
-    };
+    }; */
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
         listLocais = findViewById(R.id.idListView);
 
         //criar adaptador para lista
@@ -43,5 +47,17 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), valorSelecionado, Toast.LENGTH_LONG).show();
             }
         });
+        */
+
+        recyclerView = findViewById(R.id.idRecyclerView);
+
+        //configurar adapter
+
+
+        //  configurar recyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        //recyclerView.setAdapter();
     }
 }
