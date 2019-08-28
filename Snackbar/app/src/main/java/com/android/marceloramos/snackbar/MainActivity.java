5ebe53078d471e2ae4c46dcf1addc01762f72f13
marcelo ramos <marceloramos_86@hotmail.com>
2019-08-27 21:33:41 -0300

    Implementando Snackbar

---
+++ Snackbar/app/src/main/java/com/android/marceloramos/snackbar/MainActivity.java
@@ -0,0 +1,37 @@
+package com.android.marceloramos.snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAbrir;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonAbrir = findViewById(R.id.idButtonAbrir);
+
+        buttonAbrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Botão pressionado!", Snackbar.LENGTH_LONG).setAction("Confirmar", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonAbrir.setText("Botão alterado!");
+                    }
+
+                })
+                        .setActionTextColor(getResources().getColor(R.color.colorAccent))
+                        .show();
+            }
+        });
+    }
+}
